e867f3732b989084e4cbc6ad8f1dd8a3f9ea7284
Ngourndii-dev <hei.ainah.44@gmail.com>
2025-04-02 19:45:09 +0300

    Ajout de la vue et du template de détail blog post ainsi que du CSS pour le site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Volonteer
-
+from django.shortcuts import render, get_object_or_404
 def post_list(request):
     posts = Volonteer.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Volonteer, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
